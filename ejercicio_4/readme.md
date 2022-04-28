5f3e0c476a57c74312e828aff91857f95fc5e872
Salvi Pablo <salvipablo@gmail.com>
2022-04-27 23:32:57 -0300

    Ejercicio 4

---
+++ ejercicio_4/readme.md
@@ -0,0 +1,10 @@
+## Ejecucion del proyecto
+
+Ejecutar comando "npm i" para instalar las dependecias. Despues ejecutar comando "npm run dev" para iniciar la api
+
+***
+
+## Funcionamiento
+
+### Envio de post cob url larga, para crear url corta
+![alt text](https://github.com/salvipablo/ShipNow/blob/main/image.jpg?raw=true)
--- ejercicio_4/src/routes/urls.js
+++ ejercicio_4/src/routes/urls.js
@@ -8,9 +8,51 @@ router.get("/", (req, res) => {
   res.json(urls);
 });
 
+router.get("/urlShort", (req, res) => {
+  let dateNow = new Date();
+  let exists = false;
+  for (let i = 0; i < urls.length; i++) {
+    if (urls[i].urlShort == req.query.receivedShortUrl) {
+      exists = true;
+      let dateUrl = new Date(urls[i].createdAt);
+      days = Math.round(Math.abs(dateNow - dateUrl) / (1000 * 3600 * 24));
+      if (days > 180) res.send("Your link is expired");
+      else res.redirect(`${urls[i].urlLarge}`)
+    }
+  }
+  if (!exists) res.send("The short url sent is not found in the database");
+});
+
+router.get("/urlLarge", (req, res) => {
+  let dateNow = new Date();
+  let exists = false;
+  for (let i = 0; i < urls.length; i++) {
+    if (urls[i].urlLarge == req.query.receivedLargeUrl) {
+      exists = true;
+      let dateUrl = new Date(urls[i].createdAt);
+      days = Math.round(Math.abs(dateNow - dateUrl) / (1000 * 3600 * 24));
+      if (days > 180) res.send("Your link is expired");
+      else res.send(urls[i].urlShort);
+    }
+  }
+  if (!exists) res.send("The large url sent is not found in the database");
+});
+
 router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("post request received")
+  if (req.body.urlLarge && req.body.urlLarge != "") {
+    let nextId = urls.length + 1;
+    
+    let newUrlSave = {
+      "id": nextId,
+      "urlLarge": req.body.urlLarge,
+      "urlShort": `https://short.pj/${nextId}`,
+      "createdAt": new Date().toString()
+    };
+    urls.push(newUrlSave);
+    res.send(`saved url, this is your short url: ${newUrlSave.urlShort}`);
+  } else {
+    res.send("The url is not present, or is empty");
+  }
 })
 
 module.exports = router;
